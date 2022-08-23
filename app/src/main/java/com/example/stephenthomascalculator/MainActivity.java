d1297cb7672c77a9b88fd32728be43d7648207cd
Stephen Thomas <80729552+stephenthomas04@users.noreply.github.com>
2022-08-23 11:41:36 -0500

    8/23/22
    improved ui

--- app/src/main/java/com/example/stephenthomascalculator/MainActivity.java
+++ app/src/main/java/com/example/stephenthomascalculator/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.stephenthomascalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    public void findSum(View view) {
+        EditText number1ET = findViewById(R.id.num1ET);
+        EditText number2ET = findViewById(R.id.num2ET);
+        TextView numberSumTV = findViewById(R.id.resultTV);
+
+        int num1 = Integer.parseInt(number1ET.getText().toString());
+        int num2 = Integer.parseInt(number2ET.getText().toString());
+        int sum = num1 + num2;
+
+        numberSumTV.setText("" + sum);
+    }
+
 
 }
