6324bd765cd19b79a1eae3de42a738c4c7af39a4
Stephen Thomas <stephen.thomas042306@gmail.com>
2022-08-24 20:39:15 -0500

    8/24/22 @home
    1. Tried to fix the (android.aaptcompiler.ParsedResource@636e1e76), sadly couldn't fix it.
    2. Work on school and ask Denna/Praneet for help

--- app/src/main/java/com/example/stephenthomascalculator/MainActivity.java
+++ app/src/main/java/com/example/stephenthomascalculator/MainActivity.java
@@ -20,13 +20,53 @@ public class MainActivity extends AppCompatActivity {
         EditText number1ET = findViewById(R.id.num1ET);
         EditText number2ET = findViewById(R.id.num2ET);
         TextView numberSumTV = findViewById(R.id.resultTV);
-    //catch(Exception e)
-        int num1 = Integer.parseInt(number1ET.getText().toString());
-        int num2 = Integer.parseInt(number2ET.getText().toString());
-        int sum = num1 + num2;
-
-        numberSumTV.setText("" + sum);
+        try {
+            int num1 = Integer.parseInt(number1ET.getText().toString());
+            int num2 = Integer.parseInt(number2ET.getText().toString());
+            int sum = num1 + num2;
+            numberSumTV.setText("" + sum);
+        } catch (Exception e) {
+            numberSumTV.setText("Please enter a valid number");
+        }
     }
-
+    public void findDifference(View view) {
+        EditText number1ET = findViewById(R.id.num1ET);
+        EditText number2ET = findViewById(R.id.num2ET);
+        TextView numberSumTV = findViewById(R.id.resultTV);
+        try {
+            int num1 = Integer.parseInt(number1ET.getText().toString());
+            int num2 = Integer.parseInt(number2ET.getText().toString());
+            int difference = num1 + num2;
+            numberSumTV.setText("" + difference);
+        } catch (Exception e) {
+            numberSumTV.setText("Please enter a valid number");
+        }
+    }
+    public void findProduct(View view) {
+        EditText number1ET = findViewById(R.id.num1ET);
+        EditText number2ET = findViewById(R.id.num2ET);
+        TextView numberSumTV = findViewById(R.id.resultTV);
+        try {
+            int num1 = Integer.parseInt(number1ET.getText().toString());
+            int num2 = Integer.parseInt(number2ET.getText().toString());
+            int product = num1 + num2;
+            numberSumTV.setText("" + product);
+        } catch (Exception e) {
+            numberSumTV.setText("Please enter a valid number");
+        }
+    }
+    public void findQuotient(View view) {
+        EditText number1ET = findViewById(R.id.num1ET);
+        EditText number2ET = findViewById(R.id.num2ET);
+        TextView numberSumTV = findViewById(R.id.resultTV);
+        try {
+            int num1 = Integer.parseInt(number1ET.getText().toString());
+            int num2 = Integer.parseInt(number2ET.getText().toString());
+            int quotient = num1 + num2;
+            numberSumTV.setText("" + quotient);
+        } catch (Exception e) {
+            numberSumTV.setText("Please enter a valid number");
+        }
+    }
 
 }
